d31c62e990fcd940a28d757bcbf9fb198c24b120
KBZheng123 <53210233+KBZheng123@users.noreply.github.com>
2020-03-04 14:08:30 +0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="HTML/XHTML namespace.html">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>About us</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="css/templatemo_style.css" rel="stylesheet" type="text/css" />
+
+<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
+
+<link rel="stylesheet" type="text/css" href="css/ddsmoothmenu.css" />
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/ddsmoothmenu.js">
+/***********************************************
+* Smooth Navigational Menu- (c) Dynamic Drive DHTML code library (www.dynamicdrive.com)
+* This notice MUST stay intact for legal use
+* Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
+***********************************************/
+</script>
+
+<script type="text/javascript">
+ddsmoothmenu.init({
+    mainmenuid: "top_nav", //menu DIV id
+    orientation: 'h', //Horizontal or vertical menu: Set to "h" or "v"
+    classname: 'ddsmoothmenu', //class added to menu's outer DIV
+    //customtheme: ["#1c5a80", "#18374a"],
+    contentsource: "markup" //"markup" or ["container_id", "path_to_menu_file"]
+})
+</script>
+
+</head>
+
+<body data-gr-c-s-loaded="true"><div class="ddshadow toplevelshadow" style="left: 471.313px; top: 175px;"></div><div class="ddshadow toplevelshadow" style="left: 365.625px; top: 175px;"></div>
+
+<div id="templatemo_body_wrapper">
+<div id="templatemo_wrapper">
+
+    <div id="templatemo_header">
+        <div id="site_title"><h1><a href="about.php#">The anti-disinformation website</a></h1></div>
+    </div> <!-- END of templatemo_header -->
+    
+    <div id="templatemo_menubar">
+        <div id="top_nav" class="ddsmoothmenu">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php" class="selected">About</a></li>
+                <li style="z-index: 1000;"><a href="products.php">Site content</a>
+                    <ul style="display: none; top: 100px; visibility: visible;">
+                        <li><a href="about.php#submenu1">The prevalence of online disinformation</a></li>
+                        <li><a href="about.php#submenu2">The dangers of online disinformation</a></li>
+            <li><a href="about.php#submenu3">How to avoid online disinformation</a></li>
+                  </ul>
+                <li><a href="faqs.php">FAQs</a></li>
+                <li><a href="contact.php">Contact Us</a></li>
+            </ul>
+            <br style="clear: left">
+        </div> <!-- end of ddsmoothmenu -->
+        <div id="templatemo_search">
+            <form action="about.php#" method="get">
+              <input type="text" value=" " name="keyword" id="keyword" title="keyword" onfocus="clearText(this)" onblur="clearText(this)" class="txt_field">
+              <input type="submit" name="Search" value=" " alt="Search" id="searchbutton" title="Search" class="sub_btn">
+            </form>
+        </div>
+    </div> <!-- END of templatemo_menubar -->
+    
+    <div id="templatemo_main">
+        <div class="content_half float_r">
+            <h5>Primary Office</h5>
+            660-110 Quisque diam at ligula, <br>
+            Etiam dictum lectus quis, 11220<br>
+            Sed mattis mi at sapien<br><br>
+                        
+            Phone: 010-010-6600<br>
+            Email: <a href="mailto:info@yourcompany.com">info@yourcompany.com</a><br>
+            
+            <div class="cleaner h40"></div>
+            
+            <h5>Secondary Office</h5>
+            120-360 Cras ac nunc laoreet,<br>
+            Nulla vitae leo ac dui, 10680<br>
+            Cras id sem nulla<br><br>
+            
+            Phone: 030-030-0220<br>
+            Email: <a href="mailto:contact@yourcompany.com">contact@yourcompany.com</a><br>
+            <br>
+            Validate <a href="http://validator.w3.org/check?uri=referer" rel="nofollow">XHTML</a> &amp; <a href="http://jigsaw.w3.org/css-validator/check/referer" rel="nofollow">CSS</a>
+        </div>
+        <div class="cleaner"></div>
+    </div> <!-- END of templatemo_main -->
+    
+    <div id="templatemo_footer">
+        <p><a href="about.php#">Home</a> | <a href="about.php#">Products</a> | <a href="about.php#">About</a> | <a href="about.php#">FAQs</a> | <a href="about.php#">Checkout</a> | <a href="about.php#">Contact Us</a>
+        </p>
+        Copyright © 2072 <a href="about.php#">Your Company Name</a> <!-- Credit: www.templatemo.com -->
+        
+    </div> <!-- END of templatemo_footer -->
+    
+</div> <!-- END of templatemo_wrapper -->
+</div> <!-- END of templatemo_body_wrapper -->
+</body></html>

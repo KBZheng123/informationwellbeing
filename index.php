27c2a619504827ce9da3b10468cce708fa080edd
KBZheng123 <53210233+KBZheng123@users.noreply.github.com>
2020-03-06 19:52:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -52,7 +52,7 @@ ddsmoothmenu.init({
             <ul>
                 <li><a href="index.php" class="selected">Home</a></li>
                 <li><a href="about.php">About</a></li>
-                <li style="z-index: 100;"><a href="products.php">Site content</a>
+                <li style="z-index: 100;"><a href="#">Site content</a>
                     <ul style="display: none; top: 40px; visibility: visible;">
                         <li><a href="prevalence.php">The prevalence of online disinformation</a></li>
                         <li><a href="dangers.php">The dangers of online disinformation</a></li>

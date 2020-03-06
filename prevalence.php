60a7ac29486c341e9b7f91d1a2849eaeec2b1ef3
KBZheng123 <53210233+KBZheng123@users.noreply.github.com>
2020-03-06 11:05:58 +0700

    Add files via upload

---
+++ prevalence.php
@@ -0,0 +1,97 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="HTML/XHTML namespace.html">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>The anti-disinformation website</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="css/templatemo_style.css" rel="stylesheet" type="text/css" />
+
+<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
+
+<link rel="stylesheet" type="text/css" href="css/ddsmoothmenu.css" />
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/ddsmoothmenu.js">
+
+/***********************************************
+* Smooth Navigational Menu- (c) Dynamic Drive DHTML code library (www.dynamicdrive.com)
+* This notice MUST stay intact for legal use
+* Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
+***********************************************/
+
+</script>
+
+<script type="text/javascript">
+
+ddsmoothmenu.init({
+    mainmenuid: "top_nav", //menu DIV id
+    orientation: 'h', //Horizontal or vertical menu: Set to "h" or "v"
+    classname: 'ddsmoothmenu', //class added to menu's outer DIV
+    //customtheme: ["#1c5a80", "#18374a"],
+    contentsource: "markup" //"markup" or ["container_id", "path_to_menu_file"]
+})
+
+</script>
+
+</head>
+
+<body>
+
+<div id="templatemo_body_wrapper">
+<div id="templatemo_wrapper">
+
+    <div id="templatemo_header">
+        <div id="site_title"><h1><a href="#">The anti-disinformation page</a></h1></div>
+        <div class="cleaner"></div>
+    </div> <!-- END of templatemo_header -->
+    
+    <div id="templatemo_menubar">
+        <div id="top_nav" class="ddsmoothmenu">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li style="z-index: 100;"><a href="products.php" class="selected">Site content</a>
+                    <ul style="display: none; top: 40px; visibility: visible;">
+                        <li><a href="prevalence.php">The prevalence of online disinformation</a></li>
+                        <li><a href="about.php#submenu2">The dangers of online disinformation</a></li>
+            <li><a href="about.php#submenu3">How to avoid online disinformation</a></li>
+                  </ul>
+                <li><a href="faqs.php">FAQs</a></li>
+                <li><a href="contact.php">Contact Us</a></li>
+            </ul>
+            <br style="clear: left" />
+        </div> <!-- end of ddsmoothmenu -->
+        <div id="templatemo_search">
+            <form action="#" method="get">
+              <input type="text" value=" " name="keyword" id="keyword" title="keyword" onfocus="clearText(this)" onblur="clearText(this)" class="txt_field" />
+              <input type="submit" name="Search" value=" " alt="Search" id="searchbutton" title="Search" class="sub_btn"  />
+            </form>
+        </div>
+    </div> <!-- END of templatemo_menubar -->
+    
+    <div id="templatemo_main">
+     <div id="content" class="float_r faqs">
+            <h1>Welcome to Information wellbeing</h1>
+                    <h5>According to the Flash Eurobarometer 464 report done by the European commission, of the 26,576 people surveyed, 37% of the respondents encounter fake news almost every day, with a further 31% encountering fake news at least once a week. Only a minority of people believe they seldom ever encounter fake news.<br>
+                        According to (Westgarth, 2019), misinformation about vaccine is very common in the UK. When the Royal Society for Health interviewed parents in January 2019, it was reported that 41% of parents surveyed had been exposed to vaccine misinformation, 50% in parents with children under two years old. Facebook, according to Westgarth, is a major contributor to vaccine misinformation. Many individuals and groups may buy ad space on Facebook that promotes wrongful ideas about Vaccine. Citing the Daily Beast, Westgarth states that anti-vaccine ads have been viewed between 1.6 and 5.2 million times, mainly by women age 25 who are likely to have children. As a result of this misinformation campaign, the vaccination rate in England decreases.<br>
+                        In this digital age, whoever controls the flow of information can control public perception. As a result, many people are attempting to put wrongful information on popular websites in order to bring the public to their side. Even media giants like Twitter and YouTube are not immune to this act. According to (Avazz Report, 2020), 16% of the top 100 YouTube videos on “global warming” included under related and up-next bar contain misinformation. This means that after you watch a normal climate change video, the next video on your recommendation list has a chance to be a misinformation video. It is discovered in the report that misinformation videos are able to reach millions of views. In total, Avazz estimates that all of the videos they have reviewed add up to 21 million views, but believes that the number of videos they have not reviewed will add up more to a total of hundreds of millions of views. The YouTube’s algorithm and advertisement program play a significant role aiding the spread of misinformation. After you watch a video, YouTube is likely to recommend similar videos to you. It is estimated that 70% of the time a person spends on YouTube is on videos recommended by YouTube. Due to this, you will get more misinformation videos after watching a single one. The YouTube advertisement program does not properly check the videos to see if they contain misinformation before allows ads to run on them. As a result, climate change misinformation video creators are incentivized to keep on making more deceptive content because YouTube still allows advertisement on their videos. Due to YouTube’s tremendous reach, misinformation spread on the site can spread very quickly, causing possibly up to hundreds of millions to receive wrongful information.<br>
+                    In a similar vein, it is discovered that 25% of all tweets about climate change are posted by bot accounts, according to the Guardian. These automated accounts pretend to be real people in order to deceive real people into thinking that more people share an opinion than there actually are. The more people climate change denialists believe that share their opinion, the more strongly they hold onto their incorrect ideas. Similarly, During the 2016 U.S presidential election, it is reported that both sides utilized bots during Twitter political debates, with Trump bots significantly outnumbering Hilary bots (Kollanyi, Howard, Woodley, 2016).</h5>
+     </div>
+        <div class="cleaner"></div>
+    </div> <!-- END of templatemo_main -->
+    
+    <div id="templatemo_footer">
+        <p><a href="faqs.php#">Home</a> | <a href="faqs.php#">Products</a> | <a href="faqs.php#">About</a> | <a href="faqs.php#">FAQs</a> | <a href="faqs.php#">Checkout</a> | <a href="faqs.php#">Contact Us</a>
+        </p>
+
+        Copyright © 2072 <a href="faqs.php#">Your Company Name</a> <!-- Credit: www.templatemo.com -->
+        
+    </div>
+    
+</div> <!-- END of templatemo_wrapper -->
+</div> <!-- END of templatemo_body_wrapper -->
+
+</body>
+</html>

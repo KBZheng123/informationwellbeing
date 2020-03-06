2bb7648169c68cf49472d2aa8efc60cbdfe90da0
KBZheng123 <53210233+KBZheng123@users.noreply.github.com>
2020-03-06 11:24:49 +0700

    Add files via upload

---
+++ avoid.php
@@ -0,0 +1,108 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="HTML/XHTML namespace.html">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>The anti-disinformation website</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="css/templatemo_style.css" rel="stylesheet" type="text/css" />
+
+<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
+
+<link rel="stylesheet" type="text/css" href="css/ddsmoothmenu.css" />
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/ddsmoothmenu.js">
+
+/***********************************************
+* Smooth Navigational Menu- (c) Dynamic Drive DHTML code library (www.dynamicdrive.com)
+* This notice MUST stay intact for legal use
+* Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
+***********************************************/
+
+</script>
+
+<script type="text/javascript">
+
+ddsmoothmenu.init({
+    mainmenuid: "top_nav", //menu DIV id
+    orientation: 'h', //Horizontal or vertical menu: Set to "h" or "v"
+    classname: 'ddsmoothmenu', //class added to menu's outer DIV
+    //customtheme: ["#1c5a80", "#18374a"],
+    contentsource: "markup" //"markup" or ["container_id", "path_to_menu_file"]
+})
+
+</script>
+
+</head>
+
+<body>
+
+<div id="templatemo_body_wrapper">
+<div id="templatemo_wrapper">
+
+    <div id="templatemo_header">
+        <div id="site_title"><h1><a href="#">The anti-disinformation page</a></h1></div>
+        <div class="cleaner"></div>
+    </div> <!-- END of templatemo_header -->
+    
+    <div id="templatemo_menubar">
+        <div id="top_nav" class="ddsmoothmenu">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li style="z-index: 100;"><a href="products.php" class="selected">Site content</a>
+                    <ul style="display: none; top: 40px; visibility: visible;">
+                        <li><a href="prevalence.php">The prevalence of online disinformation</a></li>
+                        <li><a href="dangers.php">The dangers of online disinformation</a></li>
+            <li><a href="avoid.php">How to avoid online disinformation</a></li>
+                  </ul>
+                <li><a href="faqs.php">FAQs</a></li>
+                <li><a href="contact.php">Contact Us</a></li>
+            </ul>
+            <br style="clear: left" />
+        </div> <!-- end of ddsmoothmenu -->
+        <div id="templatemo_search">
+            <form action="#" method="get">
+              <input type="text" value=" " name="keyword" id="keyword" title="keyword" onfocus="clearText(this)" onblur="clearText(this)" class="txt_field" />
+              <input type="submit" name="Search" value=" " alt="Search" id="searchbutton" title="Search" class="sub_btn"  />
+            </form>
+        </div>
+    </div> <!-- END of templatemo_menubar -->
+    
+    <div id="templatemo_main">
+     <div id="content" class="float_r faqs">
+            <h1>How to avoid disinformation</h1>
+            <li>•   Nowadays, more and more people are getting news from social media instead of traditional news sources. Social media is not always a reliable source of information because anyone can share anything they want there. In addition, a study by American Press Institute has shown that people have the tendency to believe any news from people they respect, regardless of whether that news is true or not. It is better to get your news more from reputable new sites than on social media. If someone you know and/or respect share news, check from other sources to see whether the news is correct.</li>
+            <li>•   Fully read news articles you see and not just the headline: Media and new sites are known to make sensational headlines in order to attract readers. These headlines mispresent the actual facts in the news in order to make the new article more attractive to viewers, but someone who only reads title will be easily misled.</li>
+            <li>•   See if you are having a strong emotional reaction to what you are reading. News sites often embellish stories in order to elicit a strong emotional response because emotional people are likely to click on the article and/or believe in what the site is telling them. If this happen to you, try to learn more about the subject matter in the article from other sources to see how much of it is correct.</li> 
+            <li>•   Verify your sources carefully. Not every website is reliable. 
+                 <ul>
+                    <li>o   Check the domain name to make sure that it is the new sites you are looking for and not an imposter. Imposter sites often have similar name to a real site, but with a .co domain suffix.</li>
+                    <li>o   Check the web design and grammatical spelling of the site. Bad design and poor grammar may be indicative of a low-quality and untrustworthy site.</li>
+                    <li>o   Make sure that the site you are seeing is not a satire site. Satire sites are sites that intentionally post made-up stories for entertainment purpose. Usually, the site will have a notice that states that everything there are fictional. However, many people often fail to see the notice and believe that they are reading real stories. What is more dangerous is that some sites will take the information in these satire sites as facts and repose them back in their own sites as factual news.</li>
+                    <li>o   Check to see if the news you read in a site has been covered by any other sites before. Unless the site is a local news site, other sites will often cover the same story. This can be done by searching for similar keywords on online search engines.</li>
+                </ul>
+            </li>
+            <li>•   The most accurate source of information is usually firsthand sources. Firsthand sources come directly from the people involved in the event covered by the news. Some of the firsthand sources include legal documents, leaked documents, interviews, quotes, and press releases. Reputable sources tend to show these firsthand sources to prove the legitimacy of their information. Instead of reading a summary of the event as told by the news sources, you read about the event directly from firsthand sources included in the news article.</li>       
+            <li>•   It is a common occurrence that many news sites do not perform firsthand reporting, but rather report news from other sites. During this process, some pieces of information may be lost or omitted, which can lead to misinformation. To counter this, look for the first new sites that cover the event. They often contain the most complete amount of information.</li>
+            <li>•   It is always a good practice to get news from more than one source. One source may omit some information that other sources have. Reading news from multiple sources can give you a more complete picture. </li>
+            <li>•   Do not search only for what is frequently mentioned on the news. Search for what is not mentioned on the news. For instance, during the Democratic candidacy selection of 2020, Democratic candidate Bernie Sanders is mentioned far more frequently on Reddit than candidate Joe Biden, despite the fact that Biden actually holds far more delegations than Sanders. Someone who does not keep track on all candidates may be misled into thinking that Bernie is the more popular of the two.</li>
+     </div>
+        <div class="cleaner"></div>
+    </div> <!-- END of templatemo_main -->
+    
+    <div id="templatemo_footer">
+        <p><a href="faqs.php#">Home</a> | <a href="faqs.php#">Products</a> | <a href="faqs.php#">About</a> | <a href="faqs.php#">FAQs</a> | <a href="faqs.php#">Checkout</a> | <a href="faqs.php#">Contact Us</a>
+        </p>
+
+        Copyright © 2072 <a href="faqs.php#">Your Company Name</a> <!-- Credit: www.templatemo.com -->
+        
+    </div>
+    
+</div> <!-- END of templatemo_wrapper -->
+</div> <!-- END of templatemo_body_wrapper -->
+
+</body>
+</html>

ce5239bab21ae3b59b58f3cf84f0e1c38d1c57fa
KBZheng123 <53210233+KBZheng123@users.noreply.github.com>
2020-04-28 15:00:09 +0700

    Add files via upload

---
+++ resources.php
@@ -0,0 +1,98 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="HTML/XHTML namespace.html">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>The anti-disinformation website</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="css/templatemo_style.css" rel="stylesheet" type="text/css" />
+
+<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
+
+<link rel="stylesheet" type="text/css" href="css/ddsmoothmenu.css" />
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/ddsmoothmenu.js">
+
+/***********************************************
+* Smooth Navigational Menu- (c) Dynamic Drive DHTML code library (www.dynamicdrive.com)
+* This notice MUST stay intact for legal use
+* Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
+***********************************************/
+
+</script>
+
+<script type="text/javascript">
+
+ddsmoothmenu.init({
+    mainmenuid: "top_nav", //menu DIV id
+    orientation: 'h', //Horizontal or vertical menu: Set to "h" or "v"
+    classname: 'ddsmoothmenu', //class added to menu's outer DIV
+    //customtheme: ["#1c5a80", "#18374a"],
+    contentsource: "markup" //"markup" or ["container_id", "path_to_menu_file"]
+})
+
+</script>
+
+</head>
+
+<body>
+
+<div id="templatemo_body_wrapper">
+<div id="templatemo_wrapper">
+
+    <div id="templatemo_header">
+        <div id="site_title"><h1><a href="#">The anti-disinformation page</a></h1></div>
+        <div class="cleaner"></div>
+    </div> <!-- END of templatemo_header -->
+    
+    <div id="templatemo_menubar">
+        <div id="top_nav" class="ddsmoothmenu">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li style="z-index: 100;"><a href="products.php" class="selected">Site content</a>
+                    <ul style="display: none; top: 40px; visibility: visible;">
+                        <li><a href="prevalence.php">The prevalence of online disinformation</a></li>
+                        <li><a href="about.php#submenu2">The dangers of online disinformation</a></li>
+            <li><a href="about.php#submenu3">How to avoid online disinformation</a></li>
+                  </ul>
+                <li><a href="faqs.php">FAQs</a></li>
+                <li><a href="contact.php">Contact Us</a></li>
+            </ul>
+            <br style="clear: left" />
+        </div> <!-- end of ddsmoothmenu -->
+        <div id="templatemo_search">
+            <form action="#" method="get">
+              <input type="text" value=" " name="keyword" id="keyword" title="keyword" onfocus="clearText(this)" onblur="clearText(this)" class="txt_field" />
+              <input type="submit" name="Search" value=" " alt="Search" id="searchbutton" title="Search" class="sub_btn"  />
+            </form>
+        </div>
+    </div> <!-- END of templatemo_menubar -->
+    
+    <div id="templatemo_main">
+     <div id="content" class="float_r faqs">
+            <h1>Welcome to Information wellbeing</h1>
+                    <h5>Misinformation can cause people to work against their own interests and put themselves as well as future generations in danger. It may discredit ideas that have been scientifically proven and bring misconceptions into the mainstream.<br><br>
+                        According to Westgarth, diseases that have been wiped out from the U.K like measles are making a return due to anti-vaccination misinformation being spread on the Internet. When fewer and fewer people are getting vaccinated, people who cannot be vaccinated are put at risk because herd immunity no longer protects them. Herd immunity, according to Oxford vaccine group, is a concept that refers to the fact that when a majority of a community is immune to a disease, people who are not immune are safer because there are fewer people who can spread the disease to them. Another health danger of disinformation is the promotion of alternative medicine. Alternative medicine, according to cancer.org, refers to non-standard methods to treat disease that have not been verified by science.  According to the BBC, it was possible for alternative medicine companies to advertise and make videos about fake cancer cures on YouTube and receive millions of views. A survey conducted by the American Society of Clinical Oncology finds that nearly 40% of Americans believe that cancer can be cured through scientifically-unproven methods. According to a study from Yale University and cancer.org, those who use alternative medicine are shown to be 2.5 times more likely to die in a 5-year period than those who use standard treatments.<br><br>
+                    A serious issue posed by online disinformation is the infringement on users’ democratic rights. During the Federal Communications Commission’s (FCC) campaign to revoke Net Neutrality in the U.S, two million people commented in support of the FCC. Later, it was reported by multiple sources, including the BBC and Arstechnica, as many as five thousand who commented were already dead, meaning that the FCC was inflating their number of actual supporters by impersonating other people. Even in light of this discovery, the law to revoke Net Neutrality was passed, ensuring that Internet Service Providers in the United States could now control the content they wanted to show to the users. By impersonating other people to mislead the public into thinking that they had more supporters than they actually did, the FCC was able to push a law that severely limited the American internet users’ rights.<br><br>
+                    Another danger to democracy comes in the from of disinformation during election campaigns. An event known as Pizzagate happened during the 2016 presidential election, wherein presidential candidate Hillary Clinton was defamed by internet users who spread fake news of her being involved in criminal activities. Though the full extent of damage to Clinton’s campaign was not known, one has to remember that political discussion on social media is driven more by emotion than by objective facts (Barfar, 2019).  Even if the disinformation pertaining to Hillary Clinton is not true, her detractors will hate her even more, which damages her presidential campaign. Although this is one example, it is without a doubt that the act of spreading misinformation during democratic election campaigns will serve to shape many people’s perception about certain things. <br><br>
+                    Online disinformation can also lead to loss of properties. During the 2013 release of IOS 7, members of the 4Chan website started a disinformation campaign to mislead the public into believing that the new IPhone was waterproof. By creating a fake advertisement that greatly resembled a real one, 4Chan was able to mislead many into putting their IPhones underwater to test this new supposed feature. Many phones were destroyed as a result. The disinformation campaign against IOS did not end there. During the release of IOS 8, 4chan users once again attempted to start a disinformation campaign to mislead users. This time, they claimed that the new IOS 8 had the ability to charge the battery by microwaving it, causing many people to burn their IPhone when they tried to microwave it.</h5>
+     </div>
+        <div class="cleaner"></div>
+    </div> <!-- END of templatemo_main -->
+    
+    <div id="templatemo_footer">
+        <p><a href="faqs.php#">Home</a> | <a href="faqs.php#">Products</a> | <a href="faqs.php#">About</a> | <a href="faqs.php#">FAQs</a> | <a href="faqs.php#">Checkout</a> | <a href="faqs.php#">Contact Us</a>
+        </p>
+
+        Copyright © 2072 <a href="faqs.php#">Your Company Name</a> <!-- Credit: www.templatemo.com -->
+        
+    </div>
+    
+</div> <!-- END of templatemo_wrapper -->
+</div> <!-- END of templatemo_body_wrapper -->
+
+</body>
+</html>
